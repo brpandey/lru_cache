e491ec0248a9cd8ebccd3e00e2b60394056b4b7b
Bibek Pandey <bibekp@gmail.com>
2018-02-03 10:57:39 -0800

    Ran mix format

--- lib/lru/cache.ex
+++ lib/lru/cache.ex
@@ -25,27 +25,28 @@ defmodule LRU.Cache do
 
   # gb_trees (Ologn) used as lru priority queue
 
-  defstruct max_size: 0, size: 0, value_by_key: Map.new, 
-  timestamp_by_key: Map.new, lru: :gb_trees.empty()
-  
+  defstruct max_size: 0,
+            size: 0,
+            value_by_key: Map.new(),
+            timestamp_by_key: Map.new(),
+            lru: :gb_trees.empty()
+
   # The unaccessed timestamp is simply 0 instead of a 
   # millisecond time
 
-  @unaccessed_timestamp 0  
-
+  @unaccessed_timestamp 0
 
   @doc "Create a new cache given maximum number of entries"
   def new(size) when is_integer(size) and size > 0 do
     %LRU.Cache{max_size: size}
   end
 
-
   @doc "Get cache value given corresponding key"
   def get(%LRU.Cache{} = cache, key) do
-    
     # Check if key found
     case Map.has_key?(cache.value_by_key, key) do
-      false -> # return nil - consistent with Map API
+      # return nil - consistent with Map API
+      false ->
         {nil, cache}
 
       true ->
@@ -61,46 +62,49 @@ defmodule LRU.Cache do
     end
   end
 
-
   @doc """
   Put key-value pair into cache.  
   If key already exists, update only if value is different
   """
-  def put(%LRU.Cache{} = cache, key, value) when not(is_nil(key)) do
+  def put(%LRU.Cache{} = cache, key, value) when not is_nil(key) do
     # Insert key-value pair -- checking if key already found
 
     case Map.has_key?(cache.value_by_key, key) do
-      false -> 
-
+      false ->
         # Make sure we have enough room for cache entry
         # If we are at max capacity, discard the LRU entry
-        cache = cond do
-          cache.max_size == cache.size -> lru_discard(cache)
-          true -> cache 
-        end
+        cache =
+          cond do
+            cache.max_size == cache.size -> lru_discard(cache)
+            true -> cache
+          end
 
-        
         # Proceed with cache insertion
         values = Map.put(cache.value_by_key, key, value)
-        
+
         # Seed timestamp data structure and lru data structure with 
         # @unaccessed_timestamp value
         timestamps = Map.put(cache.timestamp_by_key, key, @unaccessed_timestamp)
         lru = :gb_trees.insert({@unaccessed_timestamp, key}, key, cache.lru)
 
         # Update persistent structure
-        %LRU.Cache{cache | size: cache.size + 1, lru: lru, 
-                   value_by_key: values, timestamp_by_key: timestamps}
-      
+        %LRU.Cache{
+          cache
+          | size: cache.size + 1,
+            lru: lru,
+            value_by_key: values,
+            timestamp_by_key: timestamps
+        }
+
       true ->
         # We have key, just need to determine if value is new or pre-existing
         # Size doesn't change
 
-        prior_value = Map.get(cache.value_by_key, key) 
-        
-        cond do
-          value != prior_value ->  # Pre-existing key has new value
+        prior_value = Map.get(cache.value_by_key, key)
 
+        cond do
+          # Pre-existing key has new value
+          value != prior_value ->
             # Proceed with cache insertion
             values = Map.put(cache.value_by_key, key, value)
 
@@ -109,65 +113,61 @@ defmodule LRU.Cache do
 
             # Overwrite key timestamp data to unaccessed yet
             update_timestamp(cache, key, @unaccessed_timestamp)
-            
+
           # Pre-existing key has same value -- doesn't change anything
-          true ->  cache 
+          true ->
+            cache
         end
-
     end
-
   end
-
 
   ###################
   # Helper Functions
   ###################
 
-
   # Update the access recordkeeping structures for the corresponding key
   defp update_timestamp(%LRU.Cache{} = cache, key, current_timestamp)
-  when is_integer(current_timestamp) do
-    
+       when is_integer(current_timestamp) do
     # Update the timestamp value for key and return the previous time stamp
     # Later, use previous timestamp value to index into lru data structure
 
     # Should be O(1)
 
-    {prev, timestamps} = Map.get_and_update(cache.timestamp_by_key, key, fn t -> 
-      {t, current_timestamp} # prev value, new value
-    end)
-    
+    {prev, timestamps} =
+      Map.get_and_update(cache.timestamp_by_key, key, fn t ->
+        # prev value, new value
+        {t, current_timestamp}
+      end)
+
     # Update cache struct with updated timestamps structure
     cache = Kernel.put_in(cache.timestamp_by_key, timestamps)
-    
+
     # Update the lru data structure
     # 0) Ensure the key is there
     # 1) First delete old timestamp and key
     # 2) Insert new timestamp and key
-    
+
     # Complexity should be 3*log(N) where N entries or O(log(N))
-    
+
     prev_lru_key = {prev, key}
     new_lru_key = {current_timestamp, key}
 
     case :gb_trees.lookup(prev_lru_key, cache.lru) do
-
-      {:value, _lru_value} -> # If key-value has been accessed before or initially seeded
+      # If key-value has been accessed before or initially seeded
+      {:value, _lru_value} ->
         lru = :gb_trees.delete(prev_lru_key, cache.lru)
         lru = :gb_trees.insert(new_lru_key, key, lru)
-        
+
         # Update cache struct with updated lru
         Kernel.put_in(cache.lru, lru)
-      
-      :none -> cache
-    end
 
+      :none ->
+        cache
+    end
   end
-  
 
   # Discard least recently used cache entry
   defp lru_discard(%LRU.Cache{} = cache) do
-
     # Fetch key-value pair with lowest timestamp and remove from lru
     # Should be O(log(n))
     {_smallest_k_tuple, discard_key, lru} = :gb_trees.take_smallest(cache.lru)
@@ -179,9 +179,6 @@ defmodule LRU.Cache do
     # Reflect smaller cache size
     size = cache.size - 1
 
-    %LRU.Cache{cache | size: size, lru: lru, 
-               value_by_key: values, timestamp_by_key: timestamps}
-
+    %LRU.Cache{cache | size: size, lru: lru, value_by_key: values, timestamp_by_key: timestamps}
   end
-
 end
--- test/lru/cache_test.exs
+++ test/lru/cache_test.exs
@@ -1,11 +1,9 @@
 defmodule LRU.Cache.Test do
   use ExUnit.Case, async: true
 
-
   # test values
 
   @max_size 3
-  
 
   @key1 "apple"
   @value1 "32391"
@@ -19,55 +17,42 @@ defmodule LRU.Cache.Test do
   @key4 :dill_pickle
   @value4 "167"
 
-
   describe "general cache operations not full and proper lru updates" do
-
-
     test "incorrect size" do
-      
       assert catch_error(LRU.Cache.new(-2)) == :function_clause
-
     end
 
     test "nil key" do
-      
       cache = LRU.Cache.new(2)
 
       assert catch_error(LRU.Cache.put(cache, nil, 3)) == :function_clause
-
     end
 
     test "correct insert and retrieval" do
-      
-      cache = LRU.Cache.new(@max_size) 
+      cache = LRU.Cache.new(@max_size)
 
       assert 0 = cache.size
 
-      {@value1, _c} = 
+      {@value1, _c} =
         cache
-        |> LRU.Cache.put(@key1, @value1) 
+        |> LRU.Cache.put(@key1, @value1)
         |> LRU.Cache.get(@key1)
-
     end
 
     test "insert same key same value, correct retrieval" do
-
-      {@value1, _cache} = 
-        LRU.Cache.new(@max_size) 
-        |> LRU.Cache.put(@key1, @value1) 
+      {@value1, _cache} =
+        LRU.Cache.new(@max_size)
+        |> LRU.Cache.put(@key1, @value1)
         |> LRU.Cache.put(@key1, @value1)
         |> LRU.Cache.get(@key1)
-        
     end
 
     test "insert same key different value, correct retrieval" do
-
-      cache = LRU.Cache.new(@max_size) 
+      cache = LRU.Cache.new(@max_size)
       assert {0, nil} = cache.lru
 
-      cache = cache |> LRU.Cache.put(@key1, @value1) 
+      cache = cache |> LRU.Cache.put(@key1, @value1)
       assert {1, {{0, "apple"}, "apple", nil, nil}} = cache.lru
-
 
       # After we do the first access of key1, the lru should have a non-zero
       # access time for key1
@@ -91,22 +76,17 @@ defmodule LRU.Cache.Test do
 
       assert {1, {{time, "apple"}, "apple", nil, nil}} = cache.lru
       assert time > 0
-
     end
-
   end
 
-
   describe "correct lru cache operations when full" do
-
     test "second to last element inserted is not accessed" do
-
       # 121234 (key 3 is lru, with max_size of 3)
-      
-      cache = 
-        LRU.Cache.new(@max_size) 
-        |> LRU.Cache.put(@key1, @value1) 
-        |> LRU.Cache.put(@key2, @value2) 
+
+      cache =
+        LRU.Cache.new(@max_size)
+        |> LRU.Cache.put(@key1, @value1)
+        |> LRU.Cache.put(@key2, @value2)
 
       {@value1, cache} = LRU.Cache.get(cache, @key1)
       {@value2, cache} = LRU.Cache.get(cache, @key2)
@@ -116,22 +96,23 @@ defmodule LRU.Cache.Test do
         |> LRU.Cache.put(@key3, @value3)
         |> LRU.Cache.put(@key4, @value4)
 
-      
       # key 3 has been removed
-      assert Map.equal?(%{@key1 => @value1, @key2 => @value2, @key4 => @value4}, cache.value_by_key)
-      assert {nil, _cache} = LRU.Cache.get(cache, @key3)
+      assert Map.equal?(
+               %{@key1 => @value1, @key2 => @value2, @key4 => @value4},
+               cache.value_by_key
+             )
 
+      assert {nil, _cache} = LRU.Cache.get(cache, @key3)
     end
 
     test "beginning element inserted is not accessed" do
-
       # 123234 (key 1 is lru, with max_size of 3)
-      
-      cache = 
-        LRU.Cache.new(@max_size) 
-        |> LRU.Cache.put(@key1, @value1) 
-        |> LRU.Cache.put(@key2, @value2) 
-        |> LRU.Cache.put(@key3, @value3) 
+
+      cache =
+        LRU.Cache.new(@max_size)
+        |> LRU.Cache.put(@key1, @value1)
+        |> LRU.Cache.put(@key2, @value2)
+        |> LRU.Cache.put(@key3, @value3)
 
       {@value2, cache} = LRU.Cache.get(cache, @key2)
       {@value3, cache} = LRU.Cache.get(cache, @key3)
@@ -139,21 +120,22 @@ defmodule LRU.Cache.Test do
       cache = LRU.Cache.put(cache, @key4, @value4)
 
       # key 1 has been removed
-      assert Map.equal?(%{@key2 => @value2, @key3 => @value3, @key4 => @value4}, cache.value_by_key)
+      assert Map.equal?(
+               %{@key2 => @value2, @key3 => @value3, @key4 => @value4},
+               cache.value_by_key
+             )
+
       assert {nil, _cache} = LRU.Cache.get(cache, @key1)
-
-
     end
 
     test "middle element inserted is not accessed" do
-
       # 123314 (key 2 is lru, with max_size of 3)
-      
-      cache = 
-        LRU.Cache.new(@max_size) 
-        |> LRU.Cache.put(@key1, @value1) 
-        |> LRU.Cache.put(@key2, @value2) 
-        |> LRU.Cache.put(@key3, @value3) 
+
+      cache =
+        LRU.Cache.new(@max_size)
+        |> LRU.Cache.put(@key1, @value1)
+        |> LRU.Cache.put(@key2, @value2)
+        |> LRU.Cache.put(@key3, @value3)
 
       {@value3, cache} = LRU.Cache.get(cache, @key3)
       {@value1, cache} = LRU.Cache.get(cache, @key1)
@@ -161,20 +143,22 @@ defmodule LRU.Cache.Test do
       cache = LRU.Cache.put(cache, @key4, @value4)
 
       # key 2 has been removed
-      assert Map.equal?(%{@key1 => @value1, @key3 => @value3, @key4 => @value4}, cache.value_by_key)
-      assert {nil, _cache} = LRU.Cache.get(cache, @key2)
+      assert Map.equal?(
+               %{@key1 => @value1, @key3 => @value3, @key4 => @value4},
+               cache.value_by_key
+             )
 
+      assert {nil, _cache} = LRU.Cache.get(cache, @key2)
     end
 
     test "middle element inserted is not accessed and last element inserted has same key, no discard" do
-
       # 123312 (key 2 is lru, with max_size of 3, no lru discard)
-      
-      cache = 
-        LRU.Cache.new(@max_size) 
-        |> LRU.Cache.put(@key1, @value1) 
-        |> LRU.Cache.put(@key2, @value2) 
-        |> LRU.Cache.put(@key3, @value3) 
+
+      cache =
+        LRU.Cache.new(@max_size)
+        |> LRU.Cache.put(@key1, @value1)
+        |> LRU.Cache.put(@key2, @value2)
+        |> LRU.Cache.put(@key3, @value3)
 
       {@value3, cache} = LRU.Cache.get(cache, @key3)
       {@value1, cache} = LRU.Cache.get(cache, @key1)
@@ -182,12 +166,12 @@ defmodule LRU.Cache.Test do
       cache = LRU.Cache.put(cache, @key2, @value4)
 
       # key 2 is still accessible but with new value
-      assert Map.equal?(%{@key1 => @value1, @key2 => @value4, @key3 => @value3}, cache.value_by_key)
+      assert Map.equal?(
+               %{@key1 => @value1, @key2 => @value4, @key3 => @value3},
+               cache.value_by_key
+             )
+
       assert {@value4, _cache} = LRU.Cache.get(cache, @key2)
-
     end
-
   end
-
-
 end
